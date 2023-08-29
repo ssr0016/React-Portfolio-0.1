0055ceffaec6c6b95ef75a691e527afb942f330e
ssr0016 <samsonrecluta0016@gmail.com>
2023-08-29 10:08:13 +0800

    first commit

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import './portfolio.scss'
+
+function Portfolio() {
+  return (
+    <div className='portfolio' id='portfolio'></div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/works/Works.jsx
@@ -0,0 +1,9 @@
+import './works.scss'
+
+function Works() {
+  return (
+    <div className='works' id='works'></div>
+  )
+}
+
+export default Works
